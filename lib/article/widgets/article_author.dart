4c30f99d920c052b5e94fb2f743ed6025ec57c6a
ivan_khomiak <ivan_khomiak@epam.com>
2023-08-17 01:02:05 +0300

    some UI changes

--- lib/article/widgets/article_author.dart
+++ lib/article/widgets/article_author.dart
@@ -14,9 +14,10 @@ class ArticleAuthor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 16.0,
-        vertical: 6.0,
+      padding: const EdgeInsets.only(
+        left: 16.0,
+        right: 16.0,
+        bottom: 20.0,
       ),
       child: Row(
         children: [
